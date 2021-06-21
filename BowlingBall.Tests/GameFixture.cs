db36ecf0f9d7ea07ebf42c350f6d2136a3cb43d0
Shubham Sharma <Shubham.Sharma@wu.com>
2021-06-21 11:01:44 +0530

    Add project files.

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        Game game;
+        public GameFixture()
+        {
+            game = new Game();
+        }
+
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            var game = new Game();
+            Roll(game, 0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+
+        [TestMethod]
+        public void Strike()
+        {
+            game.Roll(10);
+            game.Roll(4);
+            game.Roll(4);
+            Roll(game,17, 0);
+            Assert.AreEqual(26, game.GetScore());
+        }
+
+        [TestMethod]
+        public void Fullscore()
+        {
+            Roll(game,10, 12);
+            Assert.AreEqual(300, game.GetScore());
+        }
+
+        private void Roll(Game game, int pins, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                game.Roll(pins);
+            }
+        }
+
+
+        
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+
+        private int[] NumberofTimestoroll = new int[21];
+        
+
+        int TrackcurrentRoll = 0;
+
+        private bool HasSpare(int frameCount)
+        {
+            return NumberofTimestoroll[frameCount] + NumberofTimestoroll[frameCount + 1] == 10;
+        }
+
+        private bool HasStrike(int frameCount)
+        {
+            return NumberofTimestoroll[frameCount] == 10;
+        }
+
+       /// <summary>
+       /// Exposed to users for everytime a Pin is rolled 
+       /// </summary>
+       /// <param name="pins"> <Int> - number of pins knocked </param>
+        public void Roll(int pins)
+        {
+            NumberofTimestoroll[TrackcurrentRoll++] = pins;
+        }
+
+      
+
+        /// <summary>
+        /// Exposed for users for getting a score
+        /// </summary>
+        /// <returns></returns>
+        public int GetScore()
+        {
+            int score = 0;
+            int Index = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (HasSpare(Index))
+                {
+                    score = score + 10 + NumberofTimestoroll[Index + 2];
+                    Index = Index + 2;
+                }
+                else if (HasStrike(Index))
+                {
+                    score = score + 10 + NumberofTimestoroll[Index + 1] + NumberofTimestoroll[Index + 2];
+                    Index++;
+                }
+                else
+                {
+                    score = score +  NumberofTimestoroll[Index] + NumberofTimestoroll[Index + 1];
+                    Index = Index+ 2;
+                }
+
+            }
+
+            return score;
+        }
+       
+       
+    }
+}
